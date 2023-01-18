7566a6adf6ca9322fc208da7db507e7dc49d47d0
Rabbu-G <105190642+Rabbu-G@users.noreply.github.com>
2023-01-18 15:59:26 +0530

    .

---
+++ employee-details/src/App.tsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import Accordion from "./Components/Accordion";
+
+function App() {
+  const namelist = [
+    {
+      role: "UI Developer",
+      skillSet: "UI",
+      experience: "3",
+      location: "Hyd",
+    },
+
+    {
+      role: "React Developer",
+      skillSet: "React",
+      experience: "4",
+      location: "Hyd1",
+    },
+
+    {
+      role: "Nodejs Developer",
+      skillSet: "Node",
+      experience: "5",
+      location: "Hyd2",
+    },
+
+    {
+      role: "vue Developer",
+      skillSet: "vue",
+      experience: "2",
+      location: "Hyd3",
+    },
+  ];
+
+  // const accordionData = {
+  //   role: "UI Developer",
+  //   skillSet: "UI React",
+  //   experience: "5",
+  //   location: "location",
+  //   interviewStatus: "4 interviews scheduled",
+  //   status: "active",
+  //   clientInterivew: "yes",
+  //   comment: "interviews are in progress",
+  //   practiceHeadName: "xyz",
+  // };
+
+  return (
+    <>
+      <div className="App">
+        <Accordion names={namelist} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ employee-details/src/Components/Accordion.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from '@mui/material/Button';
+
+type PersonListsProps = {
+  names: {
+    role: string;
+    skillSet: string;
+    experience: string;
+    location: string;
+  }[];
+};
+
+export default function ControlledAccordions(props: PersonListsProps) {
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Accordion</h1>
+
+      {props.names.map((name,index) => {
+        return (
+          <Accordion
+            expanded={expanded === `panel${index}`}
+            onChange={handleChange(`panel${index}`)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel${index}bh-header"
+            >
+              <Typography sx={{ width: "20%", flexShrink: 0 }}>
+                {name.role}
+              </Typography>
+              <Typography sx={{ width: "20%", flexShrink: 0 }}>
+                {name.skillSet}
+              </Typography>
+              <Typography sx={{ width: "20%", flexShrink: 0 }}>
+                {name.experience}
+              </Typography>
+              <Typography sx={{ width: "20%", color: "text.secondary" }}>
+                {name.location}
+              </Typography>
+              <Typography sx={{ width: "10%"}}>
+              <Button variant="outlined">Edit</Button>
+              </Typography>
+
+              <Typography>
+              <Button variant="outlined">View</Button>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </div>
+  );
+}
